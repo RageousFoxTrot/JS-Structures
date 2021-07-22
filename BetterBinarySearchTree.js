a059194327389d202934854b50d9256f33d1d191
RageousFoxTrot <rageousfoxtrot@gmail.com>
2021-07-23 00:54:22 +0100

    1 file modified

--- BetterBinarySearchTree.js
+++ BetterBinarySearchTree.js
@@ -14,6 +14,35 @@ function BetterBinarySearchTree() {
       this.root = new BetterBinarySearchTreeNode(data);
       return;
     } else {
+      function searchTree(data) {
+        switch (true) {
+          case data < node.data:
+            switch (true) {
+              case node.left === null:
+                node.left = new BetterBinarySearchTreeNode(data);
+                return;
+
+              case node.right !== null:
+                return searchTree(node.left);
+            }
+            break;
+
+          case data > node.data:
+            switch (true) {
+              case node.right === null:
+                node.right = new BetterBinarySearchTreeNode(data);
+                return;
+
+              case node.right !== null:
+                return searchTree(node.rigth);
+            }
+            break;
+
+          default:
+            return null;
+        }
+      }
+
       return searchTree(node);
     }
   };
@@ -75,71 +104,68 @@ function BetterBinarySearchTree() {
   };
 
   this.remove = function(data) {
+    function removeNode(node, data) {
+      if (node === null) return null;
+
+      if (data === node.data) {
+        if (node.left === null) {
+          if (node.right === null) {
+            return null;
+          }
+
+          return node.right;
+        }
+
+        if (node.right === null) {
+          return node.left;
+        }
+
+        let $node = node.right;
+
+        while ($node.left !== null) {
+          $node = $node.left;
+        }
+        node.data = $node.data;
+        node.right = removeNode(node.right, $node.data);
+
+        return node;
+      } else if (data < node.data) {
+        node.left = remove(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    }
+
     this.root = removeNode(this.root, data);
   };
 
-  function searchTree(node) {
-    switch (true) {
-      case data < node.data:
-        switch (true) {
-          case node.left === null:
-            node.left = new BetterBinarySearchTreeNode(data);
-            return;
+  this.findMinHeight = function(node = this.root) {
+    if (node === null) return -1;
 
-          case node.right !== null:
-            return searchTree(node.left);
-        }
-        break;
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
 
-      case data > node.data:
-        switch (true) {
-          case node.right === null:
-            node.right = new BetterBinarySearchTreeNode(data);
-            return;
+    return ((left < right) ? left : right) + 1;
+  };
 
-          case node.right !== null:
-            return searchTree(node.rigth);
-        }
-        break;
+  this.findMaxHeight = function(node = this.root) {
+    if (node === null) return -1;
+    
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
 
-      default:
-        return null;
-    }
-  }
+    return ((left > right) ? left : right) + 1;
+  };
 
-  function removeNode(node, data) {
-    if (node === null) return null;
+  this.findHeightLimits = function(node = this.root) {
+    return [this.findMinHeight(), this.findMaxHeight()];
+  };
 
-    if (data === node.data) {
-      if (node.left === null) {
-        if (node.right === null) {
-          return null;
-        }
-
-        return node.right;
-      }
-
-      if (node.right === null) {
-        return node.left;
-      }
-
-      let $node = node.right;
-
-      while ($node.left !== null) {
-        $node = $node.left;
-      }
-      node.data = $node.data;
-      node.right = removeNode(node.right, $node.data);
-
-      return node;
-    } else if (data < node.data) {
-      node.left = remove(node.left, data);
-      return node;
-    } else {
-      node.right = removeNode(node.right, data);
-      return node;
-    }
-  }
+  this.isBalanced = function() {
+    return this.findMinHeight() >= this.findMaxHeight - 1;
+  };
 }
 
 export default {
